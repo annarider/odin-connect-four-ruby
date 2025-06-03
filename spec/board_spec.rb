636685271b3a364e24c4a217c034080044f7e878
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-03 14:28:23 -0600

    test: add basic test for board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+# Tests for the Connect Four Board class
+
+describe Board do
+  describe '#valid_move?' do
+    context 'when the board is empty' do
+      subject(:empty_board) { described_class.new }
+        it 'returns true for empty first column' do
+          expect(empty_board.valid_move?).to be true
+        end
+      end
+    end
+end
