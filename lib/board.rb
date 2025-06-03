601db1465f4014842980d4e777882b362fabbc3a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-03 14:35:58 -0600

    feat: add #initialize & #valid_move? methods
    Starting to code the Connect Four project using TDD
    means beginning with the Board class because there
    isn't much to test in the Player class (which will
    likely only have #initialize).
    
    Starting with a simple method in the Board class
    after #initialize by defining a #valid_move?
    method to pass the tests in board_spec.rb.

---
+++ lib/board.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Board defines a game board
+# in Connect Four.
+# 
+# It manages the state and rules 
+# of the game, such as tracking
+# the position of the game pieces,
+# checking for game over conditions,
+# and validating moves.
+#
+# @example Create a new Board
+# board = Board.new
+#
+class Board
+  attr_accessor :board
+  ROWS = 6
+  COLUMNS = 7
+
+  def initialize(rows = ROWS, columns = COLUMNS)
+    @board = Array.new(rows) { Array.new(columns)}
+  end
+
+  def valid_move?
+    true
+  end
+end
