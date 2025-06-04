9a0f9f1c9d227e9303ffe6ae7dc4ceaec8d74674
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-04 10:49:45 -0600

    style: rubocop autocorrect whitespace issues

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,15 +14,15 @@ describe Board do
     context 'when the board is empty' do
       subject(:empty_board) { described_class.new }
 
-        it 'returns true for dropping into first column' do
-          expect(empty_board.valid_move?(0)).to be true
-        end
-        it 'returns false for invalid column -1' do
-          expect(empty_board.valid_move?(-1)).to be false
-        end
-        it 'returns false for invalid column 10' do
-          expect(empty_board.valid_move?(10)).to be false
-        end
+      it 'returns true for dropping into first column' do
+        expect(empty_board.valid_move?(0)).to be true
       end
+      it 'returns false for invalid column -1' do
+        expect(empty_board.valid_move?(-1)).to be false
+      end
+      it 'returns false for invalid column 10' do
+        expect(empty_board.valid_move?(10)).to be false
+      end
+    end
   end
 end
