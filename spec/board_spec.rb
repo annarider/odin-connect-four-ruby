915134b2d2cc36fd398f3908dcd2cdfc9cf1dc23
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-04 10:22:23 -0600

    test: add check that a new board object is created

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,15 +5,16 @@ require_relative '../lib/board'
 # Tests for the Connect Four Board class
 
 describe Board do
+  it 'creates a new board' do
+    board = Board.new
+    expect(board).to be_a(Board)
+  end
   describe '#valid_move?' do
     context 'when the board is empty' do
       subject(:empty_board) { described_class.new }
 
         it 'returns true for empty first column' do
           expect(empty_board.valid_move?).to be true
-        end
-        it 'returns false for invalid moves' do
-          expect(empty_board.)
         end
       end
   end
