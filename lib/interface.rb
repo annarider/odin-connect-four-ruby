f154cf4a5297d7efa2f3eb9bdc6cdd42fbca1b42
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-10 12:07:22 -0600

    fix: change to module for better OOP
    Interface contains utility methods, has no
    instance variables, and won't need to be extended
    such that we need multiple instances. It's best
    to keep it as a module.

---
+++ lib/interface.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Interface defines a game IO
+# class in Connect Four.
+# 
+# It displays messages to the
+# user and receives input for
+# game play. It also verifies 
+# and cleanses user input.
+#
+# @example Create a new Interface
+# interface = Interface.new
+#
+class Interface
+end
---
+++ spec/interface_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative '../lib/interface'
+
+# Tests for the Connect Four Interface module
+
+describe Interface do
+end
