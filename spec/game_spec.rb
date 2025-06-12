d5c76fd5840682d0589a04603e16e6e5d302bd3e
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-12 10:21:23 -0600

    refactor: change game variable name to reuse it
    I wanted to reuse the new Game object so
    start_game no longer made sense. Changed to a
    clearer name to signal the middle of a game.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,12 +5,23 @@ require_relative '../lib/game'
 # Tests for the Connect Four Game class
 
 describe Game do
+  let(:running_game) { Game.new }
   describe '#current_player' do
-    context 'when the game starts' do
-      let(:start_game) { Game.new }
-      it "returns the current player object" do
-        expect(start_game.current_player).to be_a(Player)
+  context 'when the game starts' do
+      it 'returns the current player object' do
+        expect(running_game.current_player).to be_a(Player)
       end
     end
+  end
+
+  describe '#switch_turns' do
+    context 'when the current player ends a turn' do
+      it 'switches to the next player' do
+        current_player = running_game.current_player
+        running_game.switch_turns
+        next_player = running_game.current_player
+        expect(current_player).not_to eq(next_player)
+      end
+    end 
   end
 end
